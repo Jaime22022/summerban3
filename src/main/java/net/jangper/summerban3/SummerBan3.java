0e6ec187df026212b44d8c260cd886a3ea5823f7
Jaime22022 <anguloperezjaime@gmail.com>
2026-06-17 17:04:19 +0200

    Initial commit

---
+++ src/main/java/net/jangper/summerban3/SummerBan3.java
@@ -0,0 +1,16 @@
+package net.jangper.summerban3;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SummerBan3 implements ModInitializer {
+	public static final String MOD_ID = "summerban3";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
